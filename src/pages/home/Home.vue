3e60bdd528637fbc8093e16d13dbdd10141bf4f5
lingyu0406 <2214177889@qq.com>
2018-06-04 11:56:57 +0800

    'zaiyicitijiaoshishi'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '@/home/components/Header.vue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+  	HomeHeader
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-header">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+      msg: 'header'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="styl">
+
+</style>
